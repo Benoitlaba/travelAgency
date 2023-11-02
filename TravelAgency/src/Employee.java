a5d6bc05e4b5c52d8e3a7a39dc13bb4de4059d8e
Slarve <148897046+Benoitlaba@users.noreply.github.com>
2023-11-02 16:22:05 -0400

    Added employee toString method

--- TravelAgency/src/Employee.java
+++ TravelAgency/src/Employee.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Employee {
+import java.util.Date;
+import java.util.Objects;
+
+public class Employee extends Person{
+
+    String position;
+    public Employee(String position, String name, Date dob, String email) {
+        super(name, dob, email);
+        this.position = position;
+    }
+
+    //toString Override
+    @Override
+    public String toString() {
+        return ("This is an employee");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Employee employee = (Employee) obj;
+
+        return Objects.equals(position, employee.position);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
 }
